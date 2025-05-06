9117e43a0f9f960e9965e93b4c8202c24f9fcf95
seemapoudel58 <simapoudel6@gmail.com>
2025-05-06 20:14:22 +0545

    Add main application logic for page navigation and dataset display

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import streamlit as st
+from src.train import read
+from sections.home import show_home
+from sections.eda import show_eda
+
+st.set_page_config(page_title="Automate ML", page_icon="📊", layout="wide")
+
+if 'page' not in st.session_state:
+    st.session_state.page = "Home"  
+
+def go_to_page(page_name):
+    st.session_state.page = page_name
+    st.rerun()
+
+if st.session_state.page == "Home":
+    show_home()
+
+    if st.button('Next Page'):
+        go_to_page("EDA")
+
+elif st.session_state.page == "EDA":
+    df = st.session_state.get("df", None)  
+    if df is not None:
+        show_eda(df)
+    else:
+        st.warning("Please select a dataset from the Home page.")
+    
+    col1, col2 = st.columns([0.14, 1 ])  
+    with col1:
+        if st.button("Previous Page"):
+            go_to_page("Home")
+    with col2:
+        if st.button("Next Page"):
+            go_to_page("Model Training")
