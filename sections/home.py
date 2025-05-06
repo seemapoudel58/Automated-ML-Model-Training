6296def2d513ab9f1ace5e237c1e7c9ae6839a7a
seemapoudel58 <simapoudel6@gmail.com>
2025-05-06 16:24:51 +0545

    Refactor show_home function to include dataset selection and preview features

--- sections/home.py
+++ sections/home.py
@@ -2,7 +2,29 @@ import os
 import streamlit as st
 from src.train import read
 
-def show_home(df):  
+def show_home():  
+    st.title(" Welcome to Automate ML")
+    
+    st.markdown("""
+    This no-code ML app lets you:
+    -  Explore datasets
+    -  Perform Exploratory Data Analysis (EDA)
+    -  Train and evaluate ML models
+    """)
+
+    working_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.dirname(working_dir)
+    
+    data_dir = os.path.join(parent_dir, 'data')
+    data_list = os.listdir(data_dir)
+
+    dataset = st.selectbox(f" **Select a Dataset ** ", data_list)
+    df = read(dataset)
+
+    st.session_state["df"] = df
+    st.session_state.dataset_name = dataset
+
+
 
     if df is not None:
         st.subheader("Dataset Preview")
